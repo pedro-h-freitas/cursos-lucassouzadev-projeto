71d715351653bff22995e24ba69db41451aaa8ac
Pedro Freitas <phfreitas@ges.inatel.br>
2023-09-29 14:34:36 -0300

    feat: Adicionado react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
+import { BrowserRouter } from "react-router-dom";
+
+import { AppRoutes } from "./routes";
 
 export const App = () => {
-  return (
-    <div>
-      Teste
-    </div>
-  );
+    return (
+        <BrowserRouter>
+            <AppRoutes />
+        </BrowserRouter>
+    );
 }
