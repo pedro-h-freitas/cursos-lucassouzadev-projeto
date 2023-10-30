37fbc78c199615db5a979e22560b443ab923b3a6
Pedro Freitas <phfreitas@ges.inatel.br>
2023-10-30 15:50:43 -0300

    feat: debouncing adicionado

--- src/pages/pessoas/ListagemDePessoas.jsx
+++ src/pages/pessoas/ListagemDePessoas.jsx
@@ -4,23 +4,29 @@ import { useEffect, useMemo } from 'react';
 import { PessoasServices } from '../../shared/services/api/pessoas/PessoasServices';
 import { FerramentasDaListagem } from '../../shared/components';
 import { LayoutBasePagina } from '../../shared/layouts';
+import { useDebounce } from '../../shared/hooks';
 
 export const ListagemDePessoas = () => {
   const [searchParams, setSearchParams] = useSearchParams();
+  const { debounce } = useDebounce();
 
   const busca = useMemo(() => {
     return searchParams.get('busca') || '';
   }, [searchParams]);
 
   useEffect(() => {
-    PessoasServices.getAll(1, busca)
-      .then((result) => {
-        if (result instanceof Error) {
-          alert(result.message);
-          return;
-        }
-        console.log(result);
-      });
+
+    debounce(() => {
+      PessoasServices.getAll(1, busca)
+        .then((result) => {
+          if (result instanceof Error) {
+            alert(result.message);
+            return;
+          }
+          console.log(result);
+        });
+    });
+
   }, [busca]);
 
   return (
